d9279f56294b228a18da46506f7f285eae329518
goldsuraimu <dq0938438760@gmail.com>
2024-11-07 13:47:10 +0800

    feat: add listing page & detail page

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const path = require('path');
 const express = require('express');
+const {engine} = require('express-handlebars');
 const app = express();
 const port = 3000;
+const restaurants = require(path.join(__dirname, "public/jsons/restaurant.json")).results;
+
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.redirect('/restaurantlist')
 })
 
 app.get('/restaurantlist', (req, res) => {
-  res.send('www');
+  res.render('index', { restaurants });
 })
 
-app.get('/restaurantlist/:id', (req, res) => {
+app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id;
-  res.send(`${id}`);
+  const restaurant = restaurants.find((restaurant) => restaurant.id === Number(id))
+  res.render('detail', { restaurant });
 })
 
 app.listen(port, (req, res) => {
