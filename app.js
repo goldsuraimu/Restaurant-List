3a0eef6be90e0ec8b440af7c74a966fed4fb9359
goldsuraimu <dq0938438760@gmail.com>
2024-11-22 09:32:59 +0800

    feat: add authenticate token && request data encryption

--- app.js
+++ app.js
@@ -1,23 +1,72 @@
 const path = require('path');
 const express = require('express');
+const fs = require('fs'); 
 const {engine} = require('express-handlebars');
-const app = express();
-const port = 3000;
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
 const restaurants = require(path.join(__dirname, "public/jsons/restaurant.json")).results;
+
+// express
+const app = express();
+
+const port = 3000;
+const publicRoutes = ['/login', '/register']; //例外路由 
+const usersFilePath = path.join(__dirname, "public/jsons/userdata.json");
+
+// jwt密鑰
+const SECRET_KEY = 'this_a_secret_key';
+const TOKEN_EXPIRATION = '1h';
 
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
 
-
+// 將public設定為靜態路徑
 app.use(express.static(path.join(__dirname, 'public')));
+// 解析json格式的請求資料(body)的中介軟體
+app.use(express.json());
+//解析url
+app.use(express.urlencoded({extended: true}));
+// 解析cookie的中介軟體
+app.use(cookieParser());
+// 身分驗證中介軟體
+app.use(AuthenticateToken);
 
 app.get('/', (req, res) => {
   res.redirect('/restaurantlist') 
 })
 
+
 app.get('/login', (req, res) => {
-  res.render('login_panel', {extraCSS: "login.css"})
+  res.render('login_panel', {title: 'login | My restaurant List' , extraCSS: "login.css", extraJS: "login.js"});
+})
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const users = readUser(); //現有全部使用者名單
+  const user = users.find(u => u.username === username && u.password === password);
+
+  if (!user) {
+    return res.status(401).json({ success: false, message: "帳號或密碼錯誤" });
+  }
+
+  //密碼驗證
+  const isVaiildPassword = await bcrypt.compare(password,user.password);
+  if(!isVaiildPassword) {
+    return res.status(401).json({ success: false, message: "帳號或密碼錯誤" });
+  }
+
+  const token = jwt.sign({username: user.username}, SECRET_KEY, {expiresIn: TOKEN_EXPIRATION});
+  
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict', //防止CSRF攻擊
+    maxAge: 3600000, //1h
+  })
+
+  res.status(200).json({success: true, message: '登入成功'});
 })
 
 app.get('/restaurantlist', (req, res) => {
@@ -41,3 +90,37 @@ app.get('/restaurants/:id', (req, res) => {
 app.listen(port, (req, res) => {
   console.log(`the app is running at http://localhost:${port}`);
 })
+
+
+// function 
+
+function readUser() {
+  if(!fs.existsSync(usersFilePath)) return [];
+  return JSON.parse(fs.readFileSync(usersFilePath,'utf-8'));
+}
+
+function writeUser(user) {
+  fs.writeFileSync(usersFilePath, JSON.stringify(user,null,2));
+}
+
+function AuthenticateToken(req, res, next){
+  // 檢查是否為例外路由
+  if(publicRoutes.includes(req.path)){
+    return next();
+  }
+
+  // 驗證token
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.redirect('/login');
+  }
+
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if(err) {
+      return res.status(401).json({message: '登入憑證無效!'});
+    }
+    req.user = user;
+    next();
+  })
+}
---
+++ public/javascripts/login.js
@@ -0,0 +1,34 @@
+const SERVER_BASE_URL = 'http://localhost:3000';
+const loginPanel = document.querySelector('#login-panel');
+
+
+
+loginPanel.addEventListener('submit', async function accountSubmit(e){
+  e.preventDefault();
+
+  const username = document.querySelector('#username').value;
+  const password = document.querySelector('#password').value;
+
+
+  try {
+    const response = await fetch(`${SERVER_BASE_URL}/login`, {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({ username, password }),
+      credentials: 'include',
+    });
+
+    const data = await response.json();
+    if (data.success) {
+      alert('登入成功!');
+      window.location.href = '/restaurantlist';
+    } else {
+      alert('登入失敗:' + data.message)
+    }
+
+  } catch(err) {
+    console.log('錯誤:',err);
+    alert('發生錯誤，請稍後再試!');
+  }
+})
+
